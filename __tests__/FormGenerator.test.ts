2adf015912115101eadef276e826129b60238f24
zhenia-chugaev <zhenia.chugaev@bk.ru>
2025-04-21 12:09:17 +0200

    Add form generation class

--- __tests__/FormGenerator.test.ts
+++ __tests__/FormGenerator.test.ts
@@ -1,12 +1,9 @@
-/* eslint-disable */
-// @ts-nocheck
-
 import FormGenerator from '@/FormGenerator';
 
 test('form generation', () => {
   const template = { name: 'bob', job: 'doctor', gender: 'm' };
-  const baseForm = FormGenerator.formFor(template, {}, (f) => {});
-  const formWithUrl = FormGenerator.formFor(template, { url: '/users' }, (f) => {});
+  const baseForm = FormGenerator.formFor(template, {}, vi.fn());
+  const formWithUrl = FormGenerator.formFor(template, { url: '/users' }, vi.fn());
   expect(baseForm).toBe('<form action="#" method="post"></form>');
   expect(formWithUrl).toBe('<form action="/users" method="post"></form>');
 });
--- src/FormGenerator.ts
+++ src/FormGenerator.ts
@@ -1,5 +1,21 @@
+import { Dictionary } from '@/types';
+import Tag from './lib/Tag';
+
 class FormGenerator {
-  name = 'Form Generator';
+  private builder = {};
+
+  public static formFor(
+    template: Record<string, string>,
+    attributes: Dictionary,
+    builderFunction: (builder: object) => void
+  ): string {
+    const { builder } = new FormGenerator();
+    builderFunction(builder);
+    const { url = '#', method = 'post' } = attributes;
+    const formTag = new Tag('form', { action: url, method });
+    const formHtml = formTag.toString();
+    return formHtml;
+  }
 }
 
 export default FormGenerator;
