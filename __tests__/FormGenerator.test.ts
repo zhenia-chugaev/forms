0c63ed76c2ed99ab0d6804a793baf531a5cdaac9
zhenia-chugaev <zhenia.chugaev@bk.ru>
2025-05-04 10:50:07 +0200

    Extend form generation test suite

--- __tests__/FormGenerator.test.ts
+++ __tests__/FormGenerator.test.ts
@@ -1,9 +1,51 @@
+/* eslint-disable */
+// @ts-nocheck
+
+import { readFixture, formatHtml } from '@/utils';
 import FormGenerator from '@/FormGenerator';
 
-test('form generation', () => {
+test('form generation', async () => {
+  const template = { name: 'anna', job: 'nurse', gender: 'f' };
+  const form = FormGenerator.formFor(template, { method: 'post' }, (f) => {
+    f.input('name');
+    f.input('job', { as: 'textarea' });
+  });
+  const html = await readFixture('form1.html');
+  expect(form).toBe(formatHtml(html));
+});
+
+test('specifying additional tag attributes', async () => {
+  const template = { name: 'anna', job: 'nurse', gender: 'f' };
+  const form = FormGenerator.formFor(template, {}, (f) => {
+    f.input('name', { class: 'user-input', readonly: true });
+    f.input('job');
+  });
+  const html = await readFixture('form2.html');
+  expect(form).toBe(formatHtml(html));
+});
+
+test('overriding default attribute values', async () => {
+  const template = { name: 'anna', job: 'nurse', gender: 'f' };
+  const form = FormGenerator.formFor(template, { url: '#' }, (f) => {
+    f.input('name', { type: 'hidden' });
+    f.input('job', { as: 'textarea', rows: 50, cols: 50, minlength: 5 });
+  });
+  const html = await readFixture('form3.html');
+  expect(form).toBe(formatHtml(html));
+});
+
+test('accessing non-existent template property', () => {
   const template = { name: 'bob', job: 'doctor', gender: 'm' };
-  const baseForm = FormGenerator.formFor(template, {}, vi.fn());
-  const formWithUrl = FormGenerator.formFor(template, { url: '/users' }, vi.fn());
-  expect(baseForm).toBe('<form action="#" method="post"></form>');
-  expect(formWithUrl).toBe('<form action="/users" method="post"></form>');
+  const getForm = () =>
+    FormGenerator.formFor(template, {}, (f) => {
+      f.input('name');
+      f.input('age');
+    });
+  expect(() => getForm()).toThrow("Field 'age' does not exist in the template");
+});
+
+test('empty form generation', () => {
+  const template = { name: 'bob', job: 'doctor', gender: 'm' };
+  const form = FormGenerator.formFor(template, {}, vi.fn());
+  expect(form).toBe('<form action="#" method="post"></form>');
 });
